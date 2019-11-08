f1b734fbd688f148491b99fd2abac290b7a28f7d
Tomas Gear <tomas.e.gear@gmail.com>
2019-11-07 19:29:30 -0500

    implemented javascript to activate burger dropdown on mobile

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -10,6 +10,30 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   }
 
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+  
+      // Add a click event on each of them
+      $navbarBurgers.forEach( el => {
+        el.addEventListener('click', () => {
+  
+          // Get the target from the "data-target" attribute
+          const target = el.dataset.target;
+          const $target = document.getElementById(target);
+  
+          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+          el.classList.toggle('is-active');
+          $target.classList.toggle('is-active');
+  
+        });
+      });
+    }
+
+
   var scroll_start = 0;
   var startchange = $(".start-change");
   var offset = startchange.offset();
@@ -25,20 +49,6 @@ document.addEventListener("DOMContentLoaded", function() {
       $(".navigation-bar").addClass("navbar-change-transparent");
     }
   });
-
-  // $(".navbar-item")
-  //   .find("a")
-  //   .click(function(e) {
-  //     e.preventDefault();
-  //     var section = $(this).attr("href");
-  //     console.log(section);
-  //     $("html, body").animate(
-  //       {
-  //         scrollTop: $(section).offset().top
-  //       },
-  //       1000
-  //     );
-  //   });
 
 
   $('a[href^="#"]').on('click', function(e) {
